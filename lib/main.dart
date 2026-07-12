7b8111661bc31de2df31758e14197fdb158a0c66
toluupi <nikitakara3@gmail.com>
2026-07-12 06:03:03 +0600

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,53 @@
+import 'package:analog_clock/analog_clock.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Analog Clock',
+      home: const ClockPage(),
+    );
+  }
+}
+
+class ClockPage extends StatelessWidget {
+  const ClockPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 94, 94, 94),
+      appBar: AppBar(
+        title: const Text('Analog Clock'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 94, 94, 94),
+      ),
+      body: Center(
+        child: AnalogClock(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+          ),
+          width: 280,
+          isLive: true,
+          hourHandColor: Colors.red,
+          minuteHandColor: Colors.black,
+          secondHandColor: Colors.black,
+          numberColor: Colors.black,
+          showNumbers: true,
+          showAllNumbers: false,
+          textScaleFactor: 1.2,
+          showTicks: true,
+          showSecondHand: true,
+          showDigitalClock: false,
         ),
       ),
     );
